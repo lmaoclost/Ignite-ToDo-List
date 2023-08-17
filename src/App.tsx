8d2d1911a2302b994da3309ab40e52761424edcf
Renan Oliveira <renansmoliveira@gmail.com>
2023-08-17 12:06:57 -0300

    feat: desafio concluido

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FormEvent, useState } from 'react'
+import { v4 as uuid } from 'uuid'
+import { PlusCircle } from '@phosphor-icons/react'
+import { Header } from './components/Header'
+import { EmptyToDoList } from './components/EmptyToDoList'
+import { ToDo } from './components/ToDo'
+import { ToDoCounter } from './components/ToDoCounter'
+
+import styles from './App.module.css'
+
+interface toDoProps {
+  id: string;
+  description: string;
+  isCompleted: boolean;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [toDoList, setToDoList] = useState<toDoProps[]>([]);
+  const [toDoDescription, setToDoDescription] = useState('');
+
+  function handleToDoRegister(e: FormEvent) {
+    e.preventDefault();
+    if (toDoDescription.length > 0) {
+      const newToDo = { id: uuid(), description: toDoDescription, isCompleted: false }
+
+      setToDoList([...toDoList, newToDo]);
+      setToDoDescription('');
+    }
+  }
+
+  const removeToDoFromList = (toDoToBeRemoved: toDoProps) => {
+    const newToDoList = toDoList.filter((toDo) => {
+      return toDoToBeRemoved !== toDo
+    });
+    setToDoList(newToDoList);
+  }
+
+  const toggleStatus = (toDoToBeToggled: toDoProps) => {
+    const listWithToggledToDo = toDoList.map((toDo) => {
+      if (toDoToBeToggled === toDo) {
+        toDo.isCompleted = !toDo.isCompleted
+      }
+      return toDo
+    });
+    setToDoList(listWithToggledToDo);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Header />
+      <div className={styles.container}>
+        <main className={styles.mainContainer}>
+          <form className={styles.formContainer} onSubmit={handleToDoRegister}>
+            <input
+              placeholder="Adicione uma nova tarefa"
+              name="description"
+              id="description"
+              value={toDoDescription}
+              onChange={(e) => { setToDoDescription(e.target.value) }}
+            />
+
+            <button className={styles.createButton} type="submit">
+              <span>Criar</span>
+              <PlusCircle size={16} />
+            </button>
+          </form>
+          <ToDoCounter ToDoList={toDoList} />
+          {toDoList.length > 0 ? (
+            toDoList.map((toDoItem) => (
+              <ToDo
+                key={toDoItem.id}
+                toDo={toDoItem}
+                toggleStatus={() => toggleStatus(toDoItem)}
+                removeToDoFromList={() => removeToDoFromList(toDoItem)}
+              />
+            )
+            )
+          )
+            : <EmptyToDoList />
+          }
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
